a5cdeef753d0cf603c1048f042afb7119ae151c5
uhourri <hourri@yahoo.com>
2023-09-26 14:07:46 +0200

    Frontend added using Vue.JS

--- src/main/java/com/example/springvuejs/controller/EmployeeController.java
+++ src/main/java/com/example/springvuejs/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.example.springvuejs.controller;
 import com.example.springvuejs.entity.Employee;
 import com.example.springvuejs.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:8081")
 public class EmployeeController {
 
     @Autowired
