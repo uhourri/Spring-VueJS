0cbf45233cb27ddf0c9fd7df6a9487ae82b49b90
uhourri <hourri@yahoo.com>
2023-09-26 10:41:21 +0200

    First commit

---
+++ src/main/java/com/example/springvuejs/SpringVuejsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVuejsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVuejsApplication.class, args);
+    }
+
+}
